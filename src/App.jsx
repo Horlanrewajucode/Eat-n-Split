3bcded083c3679377a652a58b952bef78537cf39
Horlanrewajucode <buzareeolanrewaju01@gmail.com>
2025-06-24 14:05:46 +0100

    Adding new friend functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialFriends = [
   {
     id: 118836,
@@ -20,20 +22,41 @@ const initialFriends = [
 ];
 
 function App() {
+  const [showFriend, setShowFriend] = useState(false);
+  const [friends, setFriends] = useState(initialFriends);
+
+  function handleShowAddFriend() {
+    setShowFriend((showFriend) => !showFriend);
+  }
+
+  function handleAddFriend(friend) {
+    setFriends((friends) => [...friends, friend]);
+    setShowFriend(false)
+  }
+
   return (
     <div className="app">
       <div className="sidebar">
-        <FriendsList />
-        <FormAddFriend />
-        <Button>Add Friend</Button>
+        <FriendsList friends={friends} />
+        {showFriend && <FormAddFriend onAddFriend={handleAddFriend} />}
+        <Button onClick={handleShowAddFriend}>
+          {showFriend ? "Close" : "Add Friend"}
+        </Button>
       </div>
-      <FormSplitBill/>
+      <FormSplitBill />
     </div>
   );
 }
 
-function FriendsList() {
-  const friends = initialFriends;
+function Button({ children, onClick }) {
+  return (
+    <button className="button" onClick={onClick}>
+      {children}
+    </button>
+  );
+}
+
+function FriendsList({ friends }) {
   return (
     <ul>
       {friends.map((friend) => (
@@ -65,18 +88,44 @@ function Friend({ friend }) {
   );
 }
 
-function Button({ children }) {
-  return <button className="button">{children}</button>;
-}
+function FormAddFriend({ onAddFriend }) {
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("https://i.pravatar.cc/48");
 
-function FormAddFriend() {
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!name || !image) return;
+
+    const id = crypto.randomUUID();
+    const newFriend = {
+      id,
+      name,
+      image: `${image}?=${id}`,
+      balance: 0,
+    };
+
+    onAddFriend(newFriend);
+    setName("");
+    setImage("https://i.pravatar.cc/48");
+  }
   return (
-    <form className="form-add-friend">
+    <form className="form-add-friend" onSubmit={handleSubmit}>
       <label>🧑‍🤝‍🧑Friend Name:</label>
-      <input type="text" placeholder="Enter friend name" />
+      <input
+        type="text"
+        placeholder="Enter friend Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
 
       <label>🖼️Image URL:</label>
-      <input type="text" placeholder="Enter your image URL" />
+      <input
+        type="text"
+        placeholder="Enter your image URL"
+        value={image}
+        onChange={(e) => setImage(e.target.value)}
+      />
       <Button>Add</Button>
     </form>
   );
@@ -94,13 +143,17 @@ function FormSplitBill() {
       <input type="text" />
 
       <label>🧑‍🤝‍🧑X's Expense</label>
-      <input type="text" />
-      
+      <input type="text" disabled />
+
+      <label>🤑 Who is paying the Bill</label>
+      <select>
+        <option value="user">You</option>
+        <option value="friend">X</option>
+      </select>
+
       <Button>Split Bill</Button>
     </form>
   );
 }
-
-
 
 export default App;
