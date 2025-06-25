c0f45cee7e921086333051ba2f91c66d724ed4b2
Horlanrewajucode <buzareeolanrewaju01@gmail.com>
2025-06-25 09:41:41 +0100

    Added Splitting Bill Functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
 import { useState } from "react";
+import  Button  from "./Button";
+import  FriendsList  from "./FriendsList";
+import  FormAddFriend  from "./FormAddFriend";
+import  FormSplitBill  from "./FormSplitBill";
 
 const initialFriends = [
   {
@@ -39,7 +43,18 @@ function App() {
     setSelectedFriend((currSelected) =>
       currSelected?.id === friend.id ? null : friend
     );
-    setShowFriend(false)
+    setShowFriend(false);
+  }
+  function handleSplitBill(value) {
+    setFriends((friends) =>
+      friends.map((friend) =>
+        friend.id === selectedFriend.id
+          ? { ...friend, balance: friend.balance + value }
+          : friend
+      )
+    );
+
+    setSelectedFriend(null);
   }
 
   return (
@@ -57,125 +72,13 @@ function App() {
           {showFriend ? "Close" : "Add Friend"}
         </Button>
       </div>
-      {selectedFriend && <FormSplitBill selectedFriend={selectedFriend} />}
-    </div>
-  );
-}
-
-function Button({ children, onClick }) {
-  return (
-    <button className="button" onClick={onClick}>
-      {children}
-    </button>
-  );
-}
-
-function FriendsList({ friends, onSelection, selectedFriend }) {
-  return (
-    <ul>
-      {friends.map((friend) => (
-        <Friend
-          friend={friend}
-          key={friend.id}
-          onSelection={onSelection}
+      {selectedFriend && (
+        <FormSplitBill
           selectedFriend={selectedFriend}
+          onSplitBill={handleSplitBill}
         />
-      ))}
-    </ul>
-  );
-}
-
-function Friend({ friend, onSelection, selectedFriend }) {
-  const isSelected = selectedFriend?.id === friend.id;
-
-  return (
-    <li className={isSelected ? "selected" : ""}>
-      <img src={friend.image} alt={friend.name} />
-      <h3>{friend.name}</h3>
-      {friend.balance < 0 && (
-        <p className="red">
-          You owe {friend.name} {Math.abs(friend.balance)}$
-        </p>
       )}
-      {friend.balance > 0 && (
-        <p className="green">
-          {friend.name} owes you {Math.abs(friend.balance)}$
-        </p>
-      )}
-      {friend.balance === 0 && <p>you and {friend.name} are even</p>}
-
-      <Button onClick={() => onSelection(friend)}>
-        {isSelected ? "Close" : "Select"}
-      </Button>
-    </li>
-  );
-}
-
-function FormAddFriend({ onAddFriend }) {
-  const [name, setName] = useState("");
-  const [image, setImage] = useState("https://i.pravatar.cc/48");
-
-  function handleSubmit(e) {
-    e.preventDefault();
-
-    if (!name || !image) return;
-
-    const id = crypto.randomUUID();
-    const newFriend = {
-      id,
-      name,
-      image: `${image}?=${id}`,
-      balance: 0,
-    };
-
-    onAddFriend(newFriend);
-    setName("");
-    setImage("https://i.pravatar.cc/48");
-  }
-  return (
-    <form className="form-add-friend" onSubmit={handleSubmit}>
-      <label>🧑‍🤝‍🧑Friend Name:</label>
-      <input
-        type="text"
-        placeholder="Enter friend Name"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-
-      <label>🖼️Image URL:</label>
-      <input
-        type="text"
-        placeholder="Enter your image URL"
-        value={image}
-        onChange={(e) => setImage(e.target.value)}
-      />
-      <Button>Add</Button>
-    </form>
-  );
-}
-
-function FormSplitBill({ selectedFriend }) {
-  return (
-    <form className="form-split-bill">
-      <h2>Split a bill with {selectedFriend.name}</h2>
-
-      <label>💰Bill Value</label>
-      <input type="text" />
-
-      <label>🕴️Your Expense</label>
-      <input type="text" />
-
-      <label>🧑‍🤝‍🧑{selectedFriend.name}'s Expense</label>
-      <input type="text" disabled />
-
-      <label>🤑 Who is paying the Bill</label>
-      <select>
-        <option value="user">You</option>
-        <option value="friend">{selectedFriend.name}</option>
-      </select>
-
-      <Button>Split Bill</Button>
-    </form>
+    </div>
   );
 }
 
